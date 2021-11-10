745532b4b05e4adc3136ae89c973877c6149f16c
Odrian <adrian46tver@bk.ru>
2021-11-10 21:47:14 +0300

    added window resizing and display rows

--- src/main.py
+++ src/main.py
@@ -71,8 +71,9 @@ class MyWidget(QMainWindow):
         self.model1 = QStandardItemModel()
         self.model2 = QStandardItemModel()
         self.supported_types = [
-            "BIGINT", "BLOB", "BOOLEAN", "CHAR", "DATE", "DATETIME", "DECIMAL", "DOUBLE",
-            "INTEGER", "INT", "NONE", "NUMERIC", "REAL", "STRING", "TEXT", "TIME", "VARCHAR",
+            "BIGINT", "BLOB", "BOOLEAN", "CHAR", "DATE", "DATETIME",
+            "DECIMAL", "DOUBLE", "INTEGER", "INT", "NONE", "NUMERIC",
+            "REAL", "STRING", "TEXT", "TIME", "VARCHAR",
         ]
 
         self.menu_setup()
@@ -95,11 +96,11 @@ class MyWidget(QMainWindow):
         cols = ["Имя", "Тип Данных", "Внешний ключ", "Первичный ключ",
                 "Уникальность", "не Null", "По умолчанию"]
         self.model1.setHorizontalHeaderLabels(cols)
-        header = self.table1.horizontalHeader()
-        n = len(cols)
-        for i in range(0, n - 1):
-            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
-        header.setSectionResizeMode(n - 1, QHeaderView.Stretch)
+
+        self.table1.horizontalHeader().\
+            setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.table2.horizontalHeader().\
+            setSectionResizeMode(QHeaderView.ResizeToContents)
 
     def clear_tables(self):
         self.model1.setRowCount(0)
@@ -121,15 +122,18 @@ class MyWidget(QMainWindow):
             else:
                 db_info = self.get_db_info_by_widget(db)
                 self.selected = db_info
+
+                cur = db_info[4]
                 table = it.text(0)
-                columns = self.get_all_columns(db_info, table)
+                columns = self.get_all_columns(cur, table)
 
                 for column in columns:
                     self.model1.appendRow(map(QStandardItem, column.get_list()))
+                self.table1.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
                 self.model2.setHorizontalHeaderLabels(map(lambda x: x.name, columns))
-
-                # update table2
+                for row in self.get_all_data(cur, table):
+                    self.model2.appendRow(map(lambda x: QStandardItem(str(x)), row))
         except SimpleException:
             pass  # error message
 
@@ -138,45 +142,6 @@ class MyWidget(QMainWindow):
             if db_info1[2] == widget:
                 return db_info1
         raise SimpleException
-
-    def get_all_columns(self, db_info, table):
-        tex = f"SELECT sql FROM sqlite_master WHERE type='table' AND name='{table}'"
-        request = db_info[4].execute(tex).fetchall()[0][0]
-        request = request.removeprefix(f"CREATE TABLE {table} (").removesuffix(")")
-        columns = []
-        for req in request.split(', '):
-            columns.append(self.create_column(req))
-        return columns
-
-    def create_column(self, req):
-        req = req.split()
-        name = req[0]
-        column = Column(name)
-        i = 1
-        if req[i] in self.supported_types:
-            column.type = req[i]
-            i = 2
-        n = len(req)
-        while i < n:
-            if req[i] == "UNIQUE":
-                column.unique = True
-            elif i != n - 1:
-                if req[i] == "PRIMARY" and req[i + 1] == "KEY":
-                    column.primary_key = True
-                    i += 1
-                elif req[i] == "NOT" and req[i + 1] == "NULL":
-                    column.notNull = True
-                    i += 1
-                elif req[i] == "DEFAULT":
-                    column.default = req[i+1]
-                    i += 1
-                elif i != n - 2 and req[i] == "REFERENCES":
-                    link_table = req[i + 1]
-                    link_col = req[i + 2][1:-1]
-                    column.references = (link_table, link_col)
-                    i += 2
-            i += 1
-        return column
 
     def new_db(self):
         file_name = QFileDialog.getSaveFileName(
@@ -199,13 +164,55 @@ class MyWidget(QMainWindow):
         self.treeDBWidget.addTopLevelItem(widget)
         con = sqlite3.connect(path)
         cur = con.cursor()
-        tables = cur.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
-        tables = list(map(lambda x: x[0], tables))
+        tables = self.get_all_tables(cur)
         for table in tables:
             widget.addChild(QTreeWidgetItem([table]))
 
         k = [name, path, widget, con, cur, tables]
         self.dbs.append(k)
+
+    def get_all_tables(self, cur):
+        tables = cur.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+        return list(map(lambda x: x[0], tables))
+
+    def get_all_columns(self, cur, table):
+        tex = f"SELECT sql FROM sqlite_master WHERE type='table' AND name='{table}'"
+        request = cur.execute(tex).fetchall()[0][0]
+        request = request.removeprefix(f"CREATE TABLE {table} (").removesuffix(")")
+        columns = []
+        for req in request.split(', '):
+            req = req.split()
+            name = req[0]
+            column = Column(name)
+            i = 1
+            if req[i] in self.supported_types:
+                column.type = req[i]
+                i = 2
+            n = len(req)
+            while i < n:
+                if req[i] == "UNIQUE":
+                    column.unique = True
+                elif i != n - 1:
+                    if req[i] == "PRIMARY" and req[i + 1] == "KEY":
+                        column.primary_key = True
+                        i += 1
+                    elif req[i] == "NOT" and req[i + 1] == "NULL":
+                        column.notNull = True
+                        i += 1
+                    elif req[i] == "DEFAULT":
+                        column.default = req[i + 1]
+                        i += 1
+                    elif i != n - 2 and req[i] == "REFERENCES":
+                        link_table = req[i + 1]
+                        link_col = req[i + 2][1:-1]
+                        column.references = (link_table, link_col)
+                        i += 2
+                i += 1
+            columns.append(column)
+        return columns
+
+    def get_all_data(self, cur, table):
+        return cur.execute("SELECT * FROM " + table).fetchall()
 
 
 def except_hook(cls, exception, traceback):
